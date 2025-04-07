b52b708ba072aa289931453218a5f743abcd7eee
kingsofcoderz <gopinathmanjula7@gmail.com>
2025-04-07 19:39:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import random
 
 app = FastAPI()
 
-# List of premade syllables to build names from
 SYLLABLES = [
     "ka", "lo", "va", "ra", "ze", "to", "na", "mi", "shi", "ko", 
     "li", "ga", "ba", "di", "tu", "ne", "xo", "qui", "jo", "fi"
 ]
 
+ENDINGS = ["ia", "an", "ar", "stan", "land", "on", "os", "ora", "eria", "vania"]
+
 @app.get("/countryname")
-def get_country_name(syllables: int = Query(3, ge=1, le=5)):
-    name = ''.join(random.choice(SYLLABLES).capitalize() for _ in range(syllables)) + "ia"
-    return {"name": name}
+def get_country_name(
+    syllables: int = Query(3, ge=1, le=5),
+    ending: str = Query(None)
+):
+    name_body = ''.join(random.choice(SYLLABLES) for _ in range(syllables))
+    name_ending = ending if ending else random.choice(ENDINGS)
+    full_name = (name_body + name_ending).capitalize()
+    return {"name": full_name}
